663a9a44a067d0cca3418c260aec81cb863eb60d
Andru-1987 <86977326+Andru-1987@users.noreply.github.com>
2021-09-01 13:21:42 -0300

    Working the backbone of working with an Weather API Hoping next step would be the style display.

--- coding.js
+++ coding.js
@@ -1,27 +1,53 @@
 // valores del input
 let $button=document.querySelector('.button');
 let inputValue=document.querySelector('.inputValue').value;
+let header=document.querySelector('.headline');
+
 // Valores de info
 var displayCity=document.querySelector('.displayCity');
 var temperature=document.querySelector('.temperature');
+var temperatureCelsius=document.querySelector('.temperatureCelsius');
 var weatherOvercast=document.querySelector('.weatherOvercast');
 
-    console.log(inputValue);
+//Function change to Celcius degrees plus Farenheith degrees
 
+      function celsius(temp) {
+      let celsiusTemp=temp-273.15;
+      return celsiusTemp;
+      };
+
+      function faren(temp) {
+      let farenTemp=(temp*(9/5))-459.67;
+      return farenTemp;
+      };
+
+
+// retriving info releated to the Weather API
 
 $button.onclick=function(){
-
-      console.log(document.querySelector('.inputValue').value);
-      let valor =document.querySelector('.inputValue').value;
-      console.log(typeof(document.querySelector('.inputValue').value));
-      console.log(valor);
-      const url=`http://api.openweathermap.org/data/2.5/weather?q=${valor}&appid=808ed00cf43e6f99dec53060b078baea`;
-      console.log(url);
+let valor =document.querySelector('.inputValue').value;
+const url=`http://api.openweathermap.org/data/2.5/weather?q=${valor}&appid=808ed00cf43e6f99dec53060b078baea`;
       fetch(url)
       .then(response => response.json())
-      .then(data => console.log(data))
+      // .then(data => console.log(data))
+      .then(data =>
+                {
+                let nameValue=data['name'];
+
+                let tempValue=data['main']['temp'];
+                let temperatureValue=Number(tempValue);
+                let weatherOvercastValue=data['weather']['0']['description'];
+
+                displayCity.innerHTML = nameValue;
+                header.innerHTML=nameValue;
+
+                // Grados Farenheith
+                temperature.innerHTML=Math.round(faren(temperatureValue))+' ºFarenheith';
+                // Grados Celsius
+                temperatureCelsius.innerHTML=Math.round(celsius(temperatureValue)) + ' ºCelsius';
+                weatherOvercast.innerHTML=weatherOvercastValue;
+                })
+
       .catch(err => alert("Wrong city's name!"));
-      // return false;
-
-
+      return false;
 }
