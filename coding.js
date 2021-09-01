06d62f8bf2de38824840366d761004eab7f26cba
Andru-1987 <86977326+Andru-1987@users.noreply.github.com>
2021-09-01 11:59:59 -0300

    Modifications made to add the city in the url const, otherwise the fetch function will stay only into the catch error.

--- coding.js
+++ coding.js
@@ -1,7 +1,27 @@
 // valores del input
-var button=document.querySelector('.button');
-var inputValue=document.querySelector('.inputValue');
+let $button=document.querySelector('.button');
+let inputValue=document.querySelector('.inputValue').value;
 // Valores de info
 var displayCity=document.querySelector('.displayCity');
 var temperature=document.querySelector('.temperature');
 var weatherOvercast=document.querySelector('.weatherOvercast');
+
+    console.log(inputValue);
+
+
+$button.onclick=function(){
+
+      console.log(document.querySelector('.inputValue').value);
+      let valor =document.querySelector('.inputValue').value;
+      console.log(typeof(document.querySelector('.inputValue').value));
+      console.log(valor);
+      const url=`http://api.openweathermap.org/data/2.5/weather?q=${valor}&appid=808ed00cf43e6f99dec53060b078baea`;
+      console.log(url);
+      fetch(url)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => alert("Wrong city's name!"));
+      // return false;
+
+
+}
