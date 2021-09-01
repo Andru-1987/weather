a4543f5caf9f7d9833f04cdc1d1fe9d771d46a87
Andru-1987 <86977326+Andru-1987@users.noreply.github.com>
2021-09-01 10:12:22 -0300

    Uploading Files
    Start a project with APIS "weathermap"
    Not finished yet.

---
+++ coding.js
@@ -0,0 +1,7 @@
+// valores del input
+var button=document.querySelector('.button');
+var inputValue=document.querySelector('.inputValue');
+// Valores de info
+var displayCity=document.querySelector('.displayCity');
+var temperature=document.querySelector('.temperature');
+var weatherOvercast=document.querySelector('.weatherOvercast');
